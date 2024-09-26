715a4bf10b727083ad9e08be997f06b60091f7f7
David Taylor <david.taylor@snowflake.com>
2024-09-26 14:02:38 -0700

    slight upgrades to backoff script

--- examples/python_sdk/using_connections_config.py
+++ examples/python_sdk/using_connections_config.py
@@ -6,20 +6,27 @@ from snowflake.connector.config_manager import CONFIG_MANAGER
 from snowflake.core import Root
 
 # Load the Cortex Search Service name from your environment
-svc = os.environ["SNOWFLAKE_CORTEX_SEARCH_SERVICE"]
+# svc = os.environ["SNOWFLAKE_CORTEX_SEARCH_SERVICE"]
+svc = "FOMC_MINUTES_SEARCH_SERVICE"
+print(svc)
 
 # Replace with your search parameters
 query = "riding shotgun"
-columns = ["LYRIC","ALBUM_NAME","TRACK_TITLE","TRACK_N","LINE"]
+columns = ["MINUTES", "MEETING_DATE"]
 limit = 5
 
 with connect(
-      connection_name="example_connection",
+      connection_name="pm"
+) as conn:
+    session = conn.session()
+
+with connect(
+      connection_name="pm"
 ) as conn:
     try:
         # create a root as the entry point for all objects
         root = Root(conn)
-        
+        print(conn.database, conn.schema)
         response = (
             root.databases[conn.database]
             .schemas[conn.schema]
